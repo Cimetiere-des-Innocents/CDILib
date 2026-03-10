0a27c5d49a6a1a40af3c4dcc8550cd0ab441af14
Chlamydomonos <2457242458@qq.com>
2026-03-11 02:30:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CDILib
+
+CDI系列基础库
+
+本仓库无法直接构建。要参与开发，请使用[总仓库](https://github.com/Cimetiere-des-Innocents/CDIMods)。
---
+++ src/main/xyz/cimetieredesinnocents/cdilib/CDILib.kt
@@ -0,0 +1,8 @@
+package xyz.cimetieredesinnocents.cdilib
+
+import net.neoforged.fml.common.Mod
+
+@Mod(CDILib.ID)
+object CDILib {
+    const val ID = "cdilib"
+}
